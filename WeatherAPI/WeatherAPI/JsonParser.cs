522900c0e2eb0241e52e31155aa2fd65675ca222
KYA12 <21127728+KYA12@users.noreply.github.com>
2019-07-13 14:40:24 +0300

    Add files via upload

---
+++ WeatherAPI/WeatherAPI/JsonParser.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net;
+
+namespace WeatherAPI
+{
+    public class JsonParser
+    {
+        public CityList Parsing(string name)
+        {
+            CityList weather = new CityList();
+            weather.list = new List<List>();
+            string apiKey = "7d0c4218af149a91db5cb98ba3a3d22e";
+            try
+            {
+                HttpWebRequest apiRequest = WebRequest.Create("http://api.openweathermap.org/data/2.5/forecast?q=" + name + "&appid=" + apiKey + "&units=metric") as HttpWebRequest;
+                string apiResponse = "";
+                using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    apiResponse = reader.ReadToEnd();
+                }
+                weather = JsonConvert.DeserializeObject<CityList>(apiResponse);
+            }
+            
+            catch (WebException ex) when ((ex.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.NotFound)
+            {
+                weather = null;
+            }
+            catch (WebException ex)
+            {
+                weather = null;
+            }
+            return weather;
+        }
+    }
+}
---
+++ WeatherAPI/WeatherAPI/WeatherOpenMapApi.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherAPI
+{
+    public class Main
+    {
+        public double temp { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public double deg { get; set; }
+    }
+
+    public class List
+    {
+        public Main main { get; set; }
+        public List<Weather> weather { get; set; }
+        public Wind wind { get; set; }
+        public string dt_txt { get; set; }
+        [JsonProperty("rain", NullValueHandling = NullValueHandling.Ignore)]
+        public Rain rain { get; set; }
+    }
+
+    public class Coord
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+
+    public class City
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        public string name { get; set; }
+        public Coord coord { get; set; }
+        public string country { get; set; }
+    }
+
+    public class CityList
+    {
+        public List<List> list { get; set; }
+        public City city { get; set; }
+    }
+
+    public class Rain
+    {
+        [JsonProperty("3h", NullValueHandling = NullValueHandling.Ignore)]
+        public double? rain3h { get; set; }
+    }
+}
